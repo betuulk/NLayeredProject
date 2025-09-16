dd7f243036e365ed80707b82912d342f35e21dad
betuulk <btulk19@gmail.com>
2025-09-16 23:28:44 +0300

    Initial commit - N-layered project structure
    created entity, data access, business and presentation layers

---
+++ Solution1/Solution1.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution1.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; } // codefirst yaklaşımına göre veritabanında birincil anahtar ve otomatik artan olması için bu isimlendirme
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
