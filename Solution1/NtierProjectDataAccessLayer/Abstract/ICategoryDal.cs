1a3fd254116c2622fa65ef9ac5b25dba2f85f521
betuulk <btulk19@gmail.com>
2025-09-19 02:22:00 +0300

    added IGenericDal
    created various ınterfaces

---
+++ Solution1/NtierProjectDataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,13 @@
+﻿using Solution1.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NtierProjectDataAccessLayer.Abstract
+{
+    public interface ICategoryDal:IGenericDal<Category> // Category sınıfı için update ınsert işlemleri hazır hale geliyor
+    {
+    }
+}
---
+++ Solution1/NtierProjectDataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NtierProjectDataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class  // sınıf harici başka bir bileşen gelemeyecek 
+    {
+        void Insert (T entity);
+        void Update (T entity);
+        void Delete (T entity);
+        List<T> GetAll (); // tüm category türündeki verileri listeleyecek -- elektronik kategorsindeki ürünler
+        T GetById (int id); // id → Aranan kaydın primary key değeridir. id’si 346 olan Customer tablosundaki kaydı döndürür.
+
+  
+    }
+}
+// merkezi bir sistem gibi kurgulanıyor 
